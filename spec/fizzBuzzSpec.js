f427f97a7ff763950b89270732ca1167344302cc
NineInchNade <nineinchnade@gmail.com>
2014-09-25 17:08:26 +0100

    second test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -9,4 +9,12 @@ describe("FizzBuzz", function() {
       	expect(fizzBuzz.divisibleByThree(3)).toEqual(true);
   });
 
+  it("recognizes if a number is not divisible by three", function() {
+        expect(fizzBuzz.divisibleByThree(1)).toEqual(false);
+  });
+
+  it("divides by five", function() {
+        expect(fizzBuzz.divisibleByFive(5)).toEqual(true);
+  });
+
 });
